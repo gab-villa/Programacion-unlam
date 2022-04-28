b8b73fe896ca334b1f913aae14ccb9fecfab4ac7
villa <40319809+gab-villa@users.noreply.github.com>
2022-04-28 17:13:15 -0300

    Carga de novedades con retorno

---
+++ C/archivos/MaestroDeStock/trozadoArchTexto.h
@@ -0,0 +1,13 @@
+#ifndef TROZADOARCHTEXTO_H_INCLUDED
+#define TROZADOARCHTEXTO_H_INCLUDED
+
+#define VACIO '\0'
+
+#include <string.h>
+#include <stdio.h>
+#include "estructuras.h"
+
+void trozadoNovLV(char* cad, tNov* nov, char sep);
+void trozadoLF(char* cad, tProd* prod);
+
+#endif // TROZADOARCHTEXTO_H_INCLUDED
