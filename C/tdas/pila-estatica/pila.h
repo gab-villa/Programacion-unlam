ae8a1825dce74e5514086fbee8f0d1274353a57d
villa <40319809+gab-villa@users.noreply.github.com>
2022-05-22 13:08:55 -0300

    Add files via upload

---
+++ C/tdas/pila-estatica/pila.h
@@ -0,0 +1,20 @@
+#ifndef STATICPILA_H_INCLUDED
+#define STATICPILA_H_INCLUDED
+
+#include <string.h>
+#define minimo( X , Y ) ( ( X ) <= ( Y ) ? ( X ) : ( Y ) )
+#define TAM_PILA 340
+typedef struct
+{
+    char pila[TAM_PILA];
+    unsigned tope;
+} tPila;
+void crearPila(tPila *p);
+int pilaLlena(const tPila *p, unsigned cantBytes);
+int ponerEnPila(tPila *p, const void *d, unsigned cantBytes);
+int verTope(const tPila *p, void *d, unsigned cantBytes);
+int pilaVacia(const tPila *p);
+int sacarDePila(tPila *p, void *d, unsigned cantBytes);
+void vaciarPila(tPila *p);
+
+#endif // STATICPILA_H_INCLUDED
